0b171a7137777cb4b664687528a1645902348132
David BF <burretfresket@gmail.com>
2014-07-28 16:00:55 +0200

    Fix Category scaffold specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,14 @@
-require 'rails_helper'
+describe Category do
+  context 'validations' do
+    it 'has a name' do
+      subject.name = 'CategoryName'
+      expect(subject).to be_valid
+    end
 
-RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it 'has an error validating name' do
+      expect(subject).not_to be_valid
+      expect(subject.errors.any?).to eq(true)
+      expect(subject.errors.messages[:name]).not_to be_nil
+    end
+  end
 end
