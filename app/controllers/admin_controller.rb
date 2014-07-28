45e206a79a188369923f6d450cfb3c475b5c5d33
David BF <burretfresket@gmail.com>
2014-07-28 07:07:39 +0200

    Use current_user if current_resource is not defined.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -13,7 +13,7 @@ class AdminController < ApplicationController
   end
 
   def authorize_action?
-    if current_permission.allow?(params[:controller], params[:action], current_resource)
+    if current_permission.allow?(params[:controller], params[:action], current_resource || current_user)
       current_permission.permit_params! params
     else
       redirect_to root_path, alert: 'Not authorized.'
