b3105fe9f0164ea0c2e6a8103a73fba330c57741
David BF <burretfresket@gmail.com>
2014-07-31 10:24:40 +0200

    Add host to run better_errors in vagrant

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,8 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  if defined? BetterErrors
+    BetterErrors::Middleware.allow_ip! '10.0.2.2/8'
+  end
 end
