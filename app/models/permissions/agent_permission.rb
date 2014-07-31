d58878f76e4c260c3b75658e79065e8d4574361b
David BF <burretfresket@gmail.com>
2014-07-31 08:43:06 +0200

    Fix agent permissions

--- app/models/permissions/agent_permission.rb
+++ app/models/permissions/agent_permission.rb
@@ -1,15 +1,16 @@
 module Permissions
   class AgentPermission < BasePermission
     def initialize(user)
-      all_actions = [:new, :create, :show, :edit, :update, :destroy]
+      all_actions = [:index, :new, :create, :show, :edit, :update, :destroy]
 
       allow :'admin/dashboard', [:index]
       allow :'admin/users', [:new, :create]
-      allow :'admin/realestates', [:show, :edit, :update] do
-        current_user.id == current_resource.agent_id
+      allow :'admin/realestates', [:show, :edit, :update] do |current_resource|
+        user.realestate == current_resource
       end
-      allow :'admin/properties', all_actions do
-        current_user.realestate_id == current_resource.realestate_id
+      allow :'admin/properties', [:index, :new, :create]
+      allow :'admin/properties', [:show, :edit, :update, :destroy] do |current_resource|
+        user.realestate == current_resource.realestate
       end
       allow :'admin/details', all_actions
       allow :'admin/categories', all_actions
