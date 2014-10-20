638f26a7abbf11a9f38d1b0dc93330035690e283
David BF <burretfresket@gmail.com>
2014-10-20 22:28:54 +0200

    Add Category - Property assoc on Category.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
   validates_presence_of :name
+  has_many :properties_categories, class_name: 'PropertiesCategories'
+  has_many :properties, through: :properties_categories
 
   def to_s
     name
