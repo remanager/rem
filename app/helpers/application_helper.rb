80b0345a02c664231d164caa559b56181360bf58
David BF <burretfresket@gmail.com>
2014-11-04 23:43:24 +0100

    Autofill `permalink` before saving.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def generate_permalink(*args)
+    args.map(&:to_s).join('-').downcase.gsub(/\s+/, '-').gsub(/-+/, '-')
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ActiveRecord::Base
+  include ApplicationHelper
+
   validates_presence_of :name
+  before_save -> { write_attribute(:permalink, generate_permalink(id, name)) }
 
   def to_s
     name
