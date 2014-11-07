21da0f4455c179b9fec415dcb6576c138d1768ff
David BF <burretfresket@gmail.com>
2014-11-07 15:59:45 +0100

    Remove to_param from Category. Brought from other branch by mistake.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,11 @@
 class Category < ActiveRecord::Base
+  include ApplicationHelper
+
   validates_presence_of :name
   has_many :properties_categories, class_name: 'PropertiesCategories'
   has_many :properties, through: :properties_categories
 
   def to_s
     name
-  end
-
-  def to_param
-    permalink
   end
 end
