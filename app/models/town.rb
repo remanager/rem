090fb9343eb739b44db620d43b909cceda07a489
David BF <burretfresket@gmail.com>
2014-11-09 13:21:38 +0100

    Add permalink to Town.

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,5 +1,8 @@
 class Town < ActiveRecord::Base
+  include ApplicationHelper
+
   validates_presence_of :name, :postcode
+  after_create -> { update_attribute(:permalink, get_permalink(:postcode, :name)) }
 
   def to_s
     name
