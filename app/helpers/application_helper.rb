d885beeca427b3602d385ecbdf7b1613abad4ec4
David BF <burretfresket@gmail.com>
2014-11-09 13:03:18 +0100

    Generate permalink automatically after create an instance.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def get_permalink(*args)
+    args.map { |arg| self.send(arg).to_s }.join('-').gsub(/\s+/, '-').gsub(/-+/, '-')
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ActiveRecord::Base
+  include ApplicationHelper
+
   validates_presence_of :name
+  after_create -> { update_attribute(:permalink, get_permalink(:id, :name)) }
 
   def to_s
     name
