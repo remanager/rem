3b905f1c5b7b080859723cced15f36435e270c14
David BF <burretfresket@gmail.com>
2014-11-10 08:23:18 +0100

    Set relacions on Property, Realestate models.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,7 @@
 class Picture < ActiveRecord::Base
+  belongs_to Property
+  belongs_to Realestate
+  has_attached_file :image, styles: { medium: '300x300', thumb: '100x100' }
+
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
