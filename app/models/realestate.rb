530b39db8e482898f2a9baa070ff1f16156b4805
David BF <burretfresket@gmail.com>
2014-11-11 23:17:24 +0100

    Add logo to Realestate.

--- app/models/realestate.rb
+++ app/models/realestate.rb
@@ -1,7 +1,7 @@
 class Realestate < ActiveRecord::Base
   belongs_to :user
-  has_many :pictures
   has_many :properties
+  has_attachment_file :logo, styles: { thumb: '100>' }
   alias_attribute :agent, :user
 
   validates_presence_of :name
