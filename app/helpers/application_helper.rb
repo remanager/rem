8297c7d9bfda8a7e6675ae81d2f5c5c9de44c6a5
David BF <burretfresket@gmail.com>
2014-11-13 07:42:42 +0100

    Unhounding.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def get_permalink(*args)
-    args.map { |arg| self.send(arg).to_s }.join('-').gsub(/\s+/, '-').gsub(/-+/, '-')
+    args.map { |arg| send(arg).to_s }.join('-').gsub(/\s+/, '-').gsub(/-+/, '-')
   end
 end
