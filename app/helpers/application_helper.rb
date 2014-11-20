cb9e86a57fff2845f9e25094bff6e3199ca84a2d
David BF <burretfresket@gmail.com>
2014-11-21 00:38:19 +0100

    Fix get_permalink helper to have max 40 char.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def get_permalink(*args)
-    args.map { |arg| send(arg).to_s }.join('-').gsub(/\s+/, '-').gsub(/-+/, '-')
+    args.map { |arg| send(arg).to_s }.join(' ').match(/^.{0,40}\b/)[0].squeeze.gsub(/\s+/, '-').gsub(/-+/, '-')
   end
 end
