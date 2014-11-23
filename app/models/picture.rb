83c52d2b7652d0f857073679b15068456c8e5676
David BF <burretfresket@gmail.com>
2014-11-23 14:52:57 +0100

    Add image large style & size validation.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,9 @@
 class Picture < ActiveRecord::Base
   belongs_to :property
   has_one :realestate, through: :property
-  has_attached_file :image, styles: { medium: '300x150', thumb: '100x50' }
+  has_attached_file :image, styles: { large: '800x400', medium: '300x150', thumb: '100x50' }
   scope :published, -> { where(published: true) }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_size :image, size: { less_than: 1.megabytes }
 end
