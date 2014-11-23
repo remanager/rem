c235af0b1aa82dac8ba267b14fc79f42b313ae1a
David BF <burretfresket@gmail.com>
2014-11-23 17:43:13 +0100

    Style admin login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
-    if user
-      if params[:remember_me]
-        cookies.permanent[:auth_token] = user.auth_token
-      else
-        cookies[:auth_token] = user.auth_token
-      end
-      redirect_to admin_dashboard_path, notice: 'Logged in!'
+    user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
+
+    render 'new', notice: 'Invalid email or password.' unless user
+    if params[:remember_me]
+      cookies.permanent[:auth_token] = user.auth_token
     else
-      flash[:alert] = 'Invalid email or password.'
-      render :new
+      cookies[:auth_token] = user.auth_token
     end
+    redirect_to admin_dashboard_path, notice: 'Logged in!'
   end
 
   def destroy
