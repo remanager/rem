26b3758dc2a4fcbfdc58cca5c0f57fcd7375fcc1
David BF <burretfresket@gmail.com>
2014-11-23 17:09:42 +0100

    Make property (town, categories & details) random.

--- db/seeds/05_property_seeds.rb
+++ db/seeds/05_property_seeds.rb
@@ -3,15 +3,19 @@
 puts 'Seeding Property'
 
 realestate = Realestate.first
+town_ids = Town.pluck(:id)
 ncategories = Category.count
+category_ids = Category.pluck(:id)
 ndetails = Detail.count
+detail_ids = Detail.pluck(:id)
 description = <<EOF
 Esta casa es muy bonita, como todas.. ya que la descripción se repite más que el ajo,
 pero siempre esta bien poder ver un poco de texto de relleno además del clásico
 Lorem Ipsum que tanto nos ha dado =)
 EOF
 %w(Antigua Pradera CalpeRock Brillante Smells Gotelé, San-Pito-Pato, Gaviota).each do |title|
-  Property.create(title: "Casa #{ title }", realestate: realestate, town: Town.first, ref: '%04d' % rand(50),
-                  size: (rand(50) + 7) * 1000, description: description, price_sale: (rand(100) + 7) * 10000,
-                  categories: Category.first(rand(ncategories)), details: Detail.first(rand(ndetails)))
+  Property.create(title: "Casa #{ title }", realestate: realestate, town_id: town_ids.shuffle.first,
+                  ref: '%04d' % rand(50), size: (rand(50) + 7) * 1000, description: description,
+                  price_sale: (rand(100) + 7) * 10000, category_ids: category_ids.shuffle.first(rand(ncategories)),
+                  detail_ids: detail_ids.shuffle.first(rand(ndetails)))
 end
