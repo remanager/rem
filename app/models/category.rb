d08c2eb63fe2e8efa2bdd2d029a0dee506a55923
David BF <burretfresket@gmail.com>
2014-12-06 17:34:16 +0100

    Edit navbar & header.

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,7 @@ class Category < ActiveRecord::Base
   after_create -> { update_attribute(:permalink, get_permalink(:id, :name)) }
   has_many :properties_categories, class_name: 'PropertiesCategories'
   has_many :properties, through: :properties_categories
+  scope :in_menu, -> { all }
 
   def to_s
     name
